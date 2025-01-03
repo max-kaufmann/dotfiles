42b95e0d74cd9cb7fe09f73697dadd1cdd7ac125
max-kaufmann <max.a.kaufmann@gmail.com>
2025-01-03 17:01:46 -0500

    check in vector configs

---
+++ machine_specific_configs/dcms_ml_cluster/zshrc.sh
@@ -0,0 +1,7 @@
+
+
+export LD_LIBRARY_PATH=/pkgs/cuda-12.4/lib64:/pkgs/cudnn-12-v8.9.2/lib64:$LD_LIBRARY_PATH
+export PIP_CACHE_DIR=/mfs1/u/max/.cache/pip
+export PATH=$PATH:/mfs1/u/max/.conda/opt/quarto/bin
+export PATH=/pkgs/cuda-12.4/bin/:$PATH
+export PATH=/mfs1/u/max/.bin:$PATH
